4489b9494c39f108b8fb64c68c84141c3db97bdf
Shota Tamura <junkboy0315@gmail.com>
2018-01-06 23:52:41 +0900

    show initial page

--- app/src/main/java/com/yuuniworks/splitbills/MainActivity.java
+++ app/src/main/java/com/yuuniworks/splitbills/MainActivity.java
@@ -2,6 +2,7 @@ package com.yuuniworks.splitbills;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://split.yuuniworks.com/");
+
     }
 }
